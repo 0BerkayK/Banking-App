8bd128e58287c11dc90ac040ab4bd35ccd23627c
Berkay <you@example.com>
2024-06-04 04:01:45 +0300

    Commit

--- src/main/java/com/example/banking/entity/Account.java
+++ src/main/java/com/example/banking/entity/Account.java
@@ -1,8 +1,25 @@
 package com.example.banking.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Table(name = "accounts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Account {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String accountHoldername;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
     private double balance;
 }
